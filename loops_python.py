76b0d1ea3caef20e51d4d83729cb47200d81ef8e
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2025-06-07 13:50:46 +0530

    loops_py

---
+++ loops_python.py
@@ -0,0 +1,17 @@
+
+count = 1 #iterator
+while count<=5:
+    print(count)
+    # count = count + 1
+    count +=1
+
+iterator = 1
+while iterator <= 20:
+    print(iterator)
+    iterator *= 2
+
+
+
+
+
+
